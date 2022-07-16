c591ca1a18655f2284bd0eb7d276f7a20b34f64c
ch3lidze <ch3lidze@yahoo.com>
2022-07-16 16:23:44 +0400

    fetching axsios. some inf about github user

---
+++ my.js
@@ -0,0 +1,97 @@
+const APIURL = 'https://api.github.com/users/'
+const mein = document.getElementById('mein')
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+
+
+async function getUser(username) {
+    try{
+        const { data } = await axios(APIURL +
+        username)
+        createUserCard(data)
+        getRepos(username)
+    }
+    catch(err){
+        if(err.response.status == 404){
+            createErrCard('no profile this username')
+        }
+    }
+}
+
+
+async function getRepos(username){
+    try{
+        const { data } = await axios(APIURL +
+        username + '/repos')
+        addReposToCard(data)
+    }
+    catch(err){
+        if(err.response.status == 404){
+            createErrCard('no profile this username')
+        }
+    }
+}
+
+
+
+
+
+
+
+
+function createErrCard(msg){
+    const cardHTML =`
+        <div class='card'>
+            <h1>${msg}</h1>
+        </div>
+    `
+    mein.innerHTML = cardHTML
+}
+
+
+
+function createUserCard(user) {
+    const cardHTML = `
+    <div class="card"> 
+    <div> 
+        <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+    </div>
+   <div class="user-info">
+    <h2>${user.name}</h2>
+    <p>${user.bio}</p>
+    
+    <ul>
+        <li>${user.followers}<strong>Follovers</strong></li>
+        <li>${user.following}<strong>Folloving</strong></li>
+        <li>${user.public_repos}<strong>Repos</strong></li>
+    </ul>
+        <div id="repos"></div>
+    </div>
+</div>
+    `
+    mein.innerHTML = cardHTML
+}
+
+function addReposToCard(repos){
+    const reposEl = document.getElementById('repos')
+    repos
+        .slice(0,5)
+        .forEach(repo =>{
+            const repoEl = document.createElement('a')
+            repoEl.classList.add('repo')
+            repoEl.href = repo.html_url
+            repoEl.target = '_blank'
+            repoEl.innerText = repo.name
+
+            reposEl.appendChild(repoEl)
+        })
+}
+// input submit function
+form.addEventListener('submit',(e) =>{
+    e.preventDefault()
+    const user = search.value
+    if(user){
+        getUser(user)
+        search.value = ''
+    }
+})
