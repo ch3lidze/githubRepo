4bd78b1579ac187c6c517f079d772f217bb00904
ch3lidze <ch3lidze@yahoo.com>
2022-07-18 01:39:11 +0400

    I added a simple animation to the input

--- my.js
+++ my.js
@@ -1,43 +1,35 @@
-const APIURL = 'https://api.github.com/users/'
-const mein = document.getElementById('mein')
-const form = document.getElementById('form')
-const search = document.getElementById('search')
+const APIURL = 'https://api.github.com/users/';
+const mein = document.getElementById('mein');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
 
 
 async function getUser(username) {
     try{
         const { data } = await axios(APIURL +
-        username)
-        createUserCard(data)
-        getRepos(username)
+        username);
+        createUserCard(data);
+        getRepos(username);
     }
     catch(err){
         if(err.response.status == 404){
-            createErrCard('no profile this username')
-        }
-    }
-}
-
+            createErrCard('no profile this username');
+        };
+    };
+};
 
 async function getRepos(username){
     try{
         const { data } = await axios(APIURL +
-        username + '/repos')
-        addReposToCard(data)
+        username + '/repos');
+        addReposToCard(data);
     }
     catch(err){
         if(err.response.status == 404){
-            createErrCard('no profile this username')
-        }
+            createErrCard('no profile this username');
+        };
     }
 }
-
-
-
-
-
-
-
 
 function createErrCard(msg){
     const cardHTML =`
@@ -45,10 +37,8 @@ function createErrCard(msg){
             <h1>${msg}</h1>
         </div>
     `
-    mein.innerHTML = cardHTML
-}
-
-
+    mein.innerHTML = cardHTML;
+};
 
 function createUserCard(user) {
     const cardHTML = `
@@ -69,29 +59,43 @@ function createUserCard(user) {
     </div>
 </div>
     `
-    mein.innerHTML = cardHTML
-}
+    mein.innerHTML = cardHTML;
+};
 
 function addReposToCard(repos){
-    const reposEl = document.getElementById('repos')
+    const reposEl = document.getElementById('repos');
     repos
         .slice(0,5)
         .forEach(repo =>{
-            const repoEl = document.createElement('a')
-            repoEl.classList.add('repo')
-            repoEl.href = repo.html_url
-            repoEl.target = '_blank'
-            repoEl.innerText = repo.name
-
-            reposEl.appendChild(repoEl)
-        })
-}
+            const repoEl = document.createElement('a');
+            repoEl.classList.add('repo');
+            repoEl.href = repo.html_url;
+            repoEl.target = '_blank';
+            repoEl.innerText = repo.name;
+            reposEl.appendChild(repoEl);
+        });
+};
 // input submit function
 form.addEventListener('submit',(e) =>{
-    e.preventDefault()
-    const user = search.value
+    e.preventDefault();
+    const user = search.value;
     if(user){
         getUser(user)
         search.value = ''
-    }
-})
+    };
+});
+
+// placeholder simple animation
+let fSpeed = 0;
+let placeholder = "";
+const plachTxt = "Search github user";
+const speed = 100;
+
+function plchAnimate() {
+    placeholder += plachTxt.charAt(fSpeed);
+    // this search for inp id
+    search.setAttribute("placeholder",placeholder);
+    fSpeed++;
+    setTimeout(plchAnimate, speed);
+};
+plchAnimate();
